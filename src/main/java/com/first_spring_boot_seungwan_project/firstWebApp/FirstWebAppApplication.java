b2f4d6fa8a747f98bd42d0d01a780762d2d3164d
Programming-Seungwan <swsj4480@gmail.com>
2024-10-26 20:45:39 +0900

    Feat : first Java web application project settings

---
+++ src/main/java/com/first_spring_boot_seungwan_project/firstWebApp/FirstWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.first_spring_boot_seungwan_project.firstWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first_spring_boot_seungwan_project/firstWebApp/FirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first_spring_boot_seungwan_project.firstWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

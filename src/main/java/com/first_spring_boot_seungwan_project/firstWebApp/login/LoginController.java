74ae4096c9240e116d97ccdcbf82a438e859ed33
Programming-Seungwan <swsj4480@gmail.com>
2024-10-26 22:20:57 +0900

    Feat : controller & RequestMapping customization
    1. set method action by RequestMapping properties

--- src/main/java/com/first_spring_boot_seungwan_project/firstWebApp/login/LoginController.java
+++ src/main/java/com/first_spring_boot_seungwan_project/firstWebApp/login/LoginController.java
@@ -3,14 +3,21 @@ package com.first_spring_boot_seungwan_project.firstWebApp.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    @RequestMapping("login")
-    public String goToLoginPage(@RequestParam String name, ModelMap model) { // model을 통해서 원하는 데이터를 jsp 같은 view에 제공할 수 있음
-        model.put("name", name);
+    // RequestMapping의 여러 속성들을 통해 해당 컨트롤러가 특정 요청에만 반응하게 만들어줄 수도 있음
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String goToLoginPage() { // model을 통해서 원하는 데이터를 jsp 같은 view에 제공할 수 있다
         return "login";
     }
 
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
+    }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,11 @@
     <title> Login page - JSP</title>
 </head>
 <body>
-    Welcome to Login Page ${name}!
+    Welcome to Login Page!
+    <form method="POST">
+        Name : <input type="text" name="name" />
+        Password : <input type="password" name="password">
+        <input type="submit">
+    </form>
 </body>
 </html>

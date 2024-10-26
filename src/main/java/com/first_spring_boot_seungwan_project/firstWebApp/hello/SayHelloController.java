23128b0d3e2df53a11e35e1c32d9adef364357d0
Programming-Seungwan <swsj4480@gmail.com>
2024-10-26 20:59:50 +0900

    Feat : make html page by brutal way
    1. StringBuffer 클래스를 끌어와서 무식하게 적용함

--- src/main/java/com/first_spring_boot_seungwan_project/firstWebApp/hello/SayHelloController.java
+++ src/main/java/com/first_spring_boot_seungwan_project/firstWebApp/hello/SayHelloController.java
@@ -11,4 +11,19 @@ public class SayHelloController {
     public String sayHello() {
         return "Hello World!"; // 그냥 문자열을 반환하면 문자열을 이름으로하는 view를 반환한다.
     }
+
+    @RequestMapping("/say-hello-html")
+    @ResponseBody
+    public String sayHelloHTML() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>My first HTML PAGE!</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<h1>My first HTML PAGE with body!</h1>");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
 }

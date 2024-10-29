61650c1d7a0566f26f3a321779eec9a577f9856f
Programming-Seungwan <swsj4480@gmail.com>
2024-10-29 16:07:54 +0900

    Feat : add todo logic 추가

--- src/main/java/com/first_spring_boot_seungwan_project/firstWebApp/todo/TodoController.java
+++ src/main/java/com/first_spring_boot_seungwan_project/firstWebApp/todo/TodoController.java
@@ -3,8 +3,11 @@ package com.first_spring_boot_seungwan_project.firstWebApp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -24,5 +27,16 @@ public class TodoController {
         model.addAttribute("todos", todos);
 
         return "listTodos";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+    public String showNewTodoPage() {
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+    public String addNewTodoPage(@RequestParam String description, ModelMap model) {
+        todoService.addTodo((String) model.get("name"), description, LocalDate.now().plusYears(1), false);
+        return "redirect:/list-todos"; // 해당 페이지 URL로 리다이렉트 시켜줌
     }
 }
--- src/main/java/com/first_spring_boot_seungwan_project/firstWebApp/todo/TodoService.java
+++ src/main/java/com/first_spring_boot_seungwan_project/firstWebApp/todo/TodoService.java
@@ -9,13 +9,21 @@ import java.util.List;
 @Service
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>(); // 객체마다 가지는 것이 아니라 하나만 두려고 한다.
+    private static int todoCount = 0;
     static {
-        todos.add(new Todo(1, "seungwan", "learn aws", LocalDate.now().plusYears(1),false));
-        todos.add(new Todo(2, "seungwan", "learn devops", LocalDate.now().plusYears(2),false));
-        todos.add(new Todo(3, "seungwan", "learn frontend", LocalDate.now().plusYears(3),false));
+        todos.add(new Todo(++todoCount, "seungwan", "learn aws", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(++todoCount, "seungwan", "learn devops", LocalDate.now().plusYears(2),false));
+        todos.add(new Todo(++todoCount, "seungwan", "learn frontend", LocalDate.now().plusYears(3),false));
     }
 
     public List<Todo> findBUsername(String username) {
         return todos;
     }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+        Todo todo = new Todo(++todoCount, username, description, targetDate, done);
+        todos.add(todo);
+    }
+
+
 }
